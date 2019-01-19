7570c06547100a259adb1233202c44f286a0fdee
dingyuchen <dingyuchen@gmail.com>
2019-01-20 01:58:35 +0800

    complete user sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'lousy@email',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: '',
+                                         email: 'lousy@email',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
